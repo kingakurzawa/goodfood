6d90092e49e894b9616ec079231e74b90d60cedf
kingakurzawa <j.kingakurzawa@gmail.com>
2020-02-05 22:22:04 +0000

    Updates

---
+++ precache-manifest.b8af2c09a464b678648d86dddff09203.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f4690a776dbd002142ebc9234e264d",
+    "url": "/goodfood/index.html"
+  },
+  {
+    "revision": "5523013b5831c5e36d81",
+    "url": "/goodfood/static/css/main.0930cf9e.chunk.css"
+  },
+  {
+    "revision": "14a91309513bcb8806b4",
+    "url": "/goodfood/static/js/2.33ee7f44.chunk.js"
+  },
+  {
+    "revision": "a290a4b60f8d5284bd7ab92bebea836f",
+    "url": "/goodfood/static/js/2.33ee7f44.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5523013b5831c5e36d81",
+    "url": "/goodfood/static/js/main.3761ecdb.chunk.js"
+  },
+  {
+    "revision": "d93ae65c23395b120ec0",
+    "url": "/goodfood/static/js/runtime-main.2f16859c.js"
+  },
+  {
+    "revision": "cb5fcb15441452236599f928a92aa068",
+    "url": "/goodfood/static/media/bgDesktop.cb5fcb15.jpg"
+  },
+  {
+    "revision": "e83487eae2aaf8eaa621bae130be8467",
+    "url": "/goodfood/static/media/bgPhoto.e83487ea.jpg"
+  },
+  {
+    "revision": "0ceb22c5277ccf1c980be5bd2207f910",
+    "url": "/goodfood/static/media/eggs.0ceb22c5.jpg"
+  }
+]);
